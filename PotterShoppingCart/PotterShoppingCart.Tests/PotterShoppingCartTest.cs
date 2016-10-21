fe9d8b624c11022f8af6193621b9665a0a36bcc8
KeN <topken1@hotmail.com>
2016-10-22 00:01:37 +0800

    Add Tow TestCase Buy 1xEP1,2xEP2,3xEP3 TotalFee Should Be 560 Buy 1xEP1,2xEP2,3xEP3,4xEP5,5xEP5 TotalFee Should Be 1255

--- PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart/PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -122,5 +122,40 @@ namespace PotterShoppingCart.Tests
             int actual = shoppingCart.TotalFee;
             Assert.AreEqual(expected, actual);
         }
+
+
+        [TestMethod]
+        public void Test_Buy_1xEP1_2xEP2_3xEP3_TotalFee_Should_Be_560()
+        {
+            // arrange
+            ShoppingCart shoppingCart = new ShoppingCart(new ProductDao());
+            var expected = 560;
+            // act
+            shoppingCart.Add(ID: 1, quantity: 1);
+            shoppingCart.Add(ID: 2, quantity: 2);
+            shoppingCart.Add(ID: 3, quantity: 3);
+            shoppingCart.Checkout();
+            // assert
+            int actual = shoppingCart.TotalFee;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Buy_1xEP1_2xEP2_3xEP3_4xEP4_5xEP5_TotalFee_Should_Be_1255()
+        {
+            // arrange
+            ShoppingCart shoppingCart = new ShoppingCart(new ProductDao());
+            var expected = 1255;
+            // act
+            shoppingCart.Add(ID: 1, quantity: 1);
+            shoppingCart.Add(ID: 2, quantity: 2);
+            shoppingCart.Add(ID: 3, quantity: 3);
+            shoppingCart.Add(ID: 4, quantity: 4);
+            shoppingCart.Add(ID: 5, quantity: 5);
+            shoppingCart.Checkout();
+            // assert
+            int actual = shoppingCart.TotalFee;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
